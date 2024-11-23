6d986e8e96276702aec81e1028ddb42d2dd5cecb
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-23 14:14:44 +0530

    commit

---
+++ first.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+using namespace std;
+
+int main(){
+    puts("Click on Button");
+    puts("Create a new player");
+    puts("add life to player");
+    return 0;
+}
+
+
+// #include <iostream>
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     cout << "Hello World" ;
+//     return 0;
+// }
