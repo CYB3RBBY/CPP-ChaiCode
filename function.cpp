d5e5c249cde7ad543dac505cedacbe5f13f40955
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-25 18:59:48 +0530

    function

---
+++ function.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// function
+int sum(int a, int b){
+    int c ;
+    c = a + b;
+    return c;
+}
+int main (){
+    int a , b;
+    cout << "Enter the first number:" << endl;
+    cin >> a;
+    cout << "Enter the second number:" << endl;
+    cin >> b;
+    cout << "The function returned " << sum(a,b) << endl;
+    return 0;
+}
