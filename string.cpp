28f7a8adc41ceafd66b237f711bad0658b820e1c
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-25 19:32:47 +0530

    add changes

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+int main (){
+    string name = "aishwarya";
+    cout << "The name is " << name << endl;
+    cout << "The length of name is " << name.length() << endl;
+    cout << "The name is " << name.substr(0,3) << endl;
+    
+    return 0;
+}
