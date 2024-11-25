5bb6525ac438b2a20096880638c0e7a510b7dc01
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-25 17:54:22 +0530

    add changes

---
+++ dataTypes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int integerDatatype = 10; // takes integer
+    short sa = 9 ;
+    int b;
+    long c;
+    long long d ;
+    // cout << sa;
+ 
+    float const score = 45.6; // constant value can't be reassinged 
+    double score1 = 455.4;
+    long double score4 = 56.678;
+    // score = 10; // reasings the value of variable
+    score1 = 23;
+    cout << "the score is " << score << endl;
+
+
+    // camel case notation 
+
+     int marksInMaths = 83;
+     cout << "the marks of the students in maths are " << marksInMaths;
+    return 0;
+}
