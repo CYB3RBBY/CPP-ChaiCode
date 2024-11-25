0b2f8bb740ac06f9579e1d0fe1e3f7a27b674f0e
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-25 18:06:29 +0530

    add changes

---
+++ takingInput.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Taking inputs and operators 
+int main(){
+    float a , b; // typecasting because can't use division operation on integer values
+    cout << "Enter first number:" << endl;
+    cin >> a;
+    cout << "Enter second number:" << endl;
+    cin >> b;
+    
+    cout << "The sum is " << a + b << endl;
+    cout << "a + b is: " << a + b << endl;
+    cout << "a - b is: " << a - b << endl;
+    cout << "a * b is: " << a * b << endl;
+    cout << "a / b is: " << a / b << endl;
+
+    return 0;
+}
