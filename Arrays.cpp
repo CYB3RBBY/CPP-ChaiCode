61c36315b440120fe6c7d0cdd88e493dc0f28c92
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-25 19:12:49 +0530

    add changes

--- Arrays.cpp
+++ Arrays.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main (){
-    int arr[3] = { 1 , 2, 3};
+    int marks[6] = { 100 , 100 , 98 , 99 , 98 , 99};
+    for ( int i = 0; i < 6; i++ ){
+        cout << "marks of " << i+1 << " student are: " << marks[i] << endl;
+    }
     return 0;
 }
