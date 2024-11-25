c2cb3aecfa7b7db6bcafffaed7e58022730e26c7
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-25 19:27:30 +0530

    add changes

---
+++ typescript.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int a = 343;
+    float b = 87.94;
+    cout << (float) a/34<<endl;
+
+    cout << (int) b ;
+    // no round off
+    return 0;
+}
