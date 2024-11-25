da5651e34e4351e7aa80655ec2003a53990218be
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-25 19:23:50 +0530

    add changes

--- Arrays.cpp
+++ Arrays.cpp
@@ -2,9 +2,21 @@
 using namespace std;
 
 int main (){
-    int marks[6] = { 100 , 100 , 98 , 99 , 98 , 99};
-    for ( int i = 0; i < 6; i++ ){
-        cout << "marks of " << i+1 << " student are: " << marks[i] << endl;
+    // int marks[6] = { 100 , 100 , 98 , 99 , 98 , 99};
+    // for ( int i = 0; i < 6; i++ ){
+    //     cout << "marks of " << i+1 << " student are: " << marks[i] << endl;
+    // }
+
+    int arr2d[2][3] = {
+        {1,2,3},
+        {4,5,6}
+    };
+    for ( int i = 0; i < 2; i++){
+        for (int j = 0; j < 3; j++){
+            cout << "the value at " << i << ", " << j << " is " << arr2d[i][j] << endl;
+        }
     }
+
+
     return 0;
 }
