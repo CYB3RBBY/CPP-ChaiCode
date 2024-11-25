45dba16f86331317313f332821b42d7cecb68be9
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-25 20:32:33 +0530

    add changes

---
+++ Constructer.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+class Employee {
+    public:
+    string name;
+    int salary;
+
+    Employee(string name, int salary){
+        this->name = name;
+        this->salary = salary;
+    }
+
+    void printDetails(){
+cout << " The name of our first employee is " << this->name << " and her salary is " << this->salary << endl;
+    }
+};
+
+Employee Ash("Ashwarya constructor", 344);
+// Ash.name = "Ashwarya";
+// Ash.salary = 1000000;
+Ash.printDetails();
+// cout << " The name of our first employee is " << Ash.name << " and her salary is " << Ash.salary << endl;
+
+    return 0;
+}
