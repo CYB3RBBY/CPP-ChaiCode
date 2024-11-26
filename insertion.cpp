5c2ef36a723be994fb7217c6b214438c9f609140
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-26 17:53:11 +0530

    add changes

--- insertion.cpp
+++ insertion.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void display(int arr[], int n) {
     // Traversal 
     for (int i = 0; i < n; i++) {
-        printf("%d\n", arr[i]);
+        cout << arr[i] << endl;
     }
 }
 
