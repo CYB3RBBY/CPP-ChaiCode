9ec325089565f6b08369fb4e6d0977a4338993cf
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-26 17:16:57 +0530

    add changes

--- insertion.cpp
+++ insertion.cpp
@@ -3,23 +3,32 @@ using namespace std;
 
 void display(int arr[], int n) {
     // Traversal 
-   for (int i = 0; i < n; i++) {
-       printf("%d\n", arr[i]);
-   }
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr[i]);
+    }
 }
 
-void indInsertion(int arr[] ,int size ,int element ,int capacity, int index){
-     if(size>=capacity){
+// Insertion
+void indInsertion(int arr[], int size, int element, int capacity, int index) {
+    if (size >= capacity) {
+        cout << "Insertion failed: Array is at full capacity" << endl;
         return;
-     }
+    }
+    for (int i = size - 1; i >= index; i--) {
+        arr[i + 1] = arr[i];
+    }
+    arr[index] = element;
 }
 
 int main() {
-    int arr[100] = {1, 2, 6, 78};
-    int size = 4;
+    int arr[100] = {7, 8, 12, 27, 88};
+    int size = 5;
     int element = 45;
     int index = 3;
+
+    indInsertion(arr, size, element, 100, index);
+    size += 1;
     display(arr, size);
-    indInsertion(arr , size , element , 100, index);
+
     return 0;
 }
