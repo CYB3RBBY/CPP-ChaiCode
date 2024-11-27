4924e17f4823d11d928d59f4bcd73c56130226f6
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-27 13:31:27 +0530

    add changes

---
+++ LinearBinarySearch.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int linearSearch(int arr[], int size, int element){
+    for (int i = 0; i < size; i++){
+        if(arr[i]==element){
+            return i;
+        }
+    }
+    return -1;
+}
+int main(){
+    int arr[] = {1,3,5,56,4,3,23,5,4,54634,56,34};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int element = 4; 
+    int searchIndex = linearSearch(arr, size ,4);
+    cout << "The element " << element << " was fount at index " << searchIndex << endl;
+    return 0;
+}
