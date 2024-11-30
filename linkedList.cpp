6d2885dd582164507be5ccf38e96f8a47dfdf94a
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-11-30 23:57:34 +0530

    add changes

---
+++ linkedList.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+int main()
+{
+    struct Node *head;
+    struct Node * second;
+    struct Node * third;
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second =
+    return 0;
+}
