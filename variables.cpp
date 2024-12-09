f1287513a30b440d8e6a8f10b36650cc21810df4
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-12-09 21:48:09 +0530

    add changes

---
+++ variables.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float annualSalary = 50000.99;
+    float monthlySalary = annualSalary / 12;
+    cout << "Your monthly salary is " << monthlySalary << endl;
+    system("pause>0");
+}
