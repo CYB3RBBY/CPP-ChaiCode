b5f5faeb346f5c235e2e786bcc014cd1e7948934
CYB3RBBY <aishwaryadhyani04@gmail.com>
2024-12-10 09:06:13 +0530

    add changes

---
+++ datatypeOverflow.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int intMax = INT_MAX;
+    cout << "Integer Maximum: " << intMax << endl;
+    cout << "Datatype Overflow: " << intMax + 1 << endl;
+    return 0;
+    // Output: Integer Maximum: 2147483647
+}
